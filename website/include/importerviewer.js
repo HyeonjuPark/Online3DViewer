426977d817ab606a8d8eb5aa26aa4706f2e1c71c
hjpark <imcreator3@gmail.com>
2019-12-16 14:22:54 +0900

    emissive 설정 일시 중지

--- website/include/importerviewer.js
+++ website/include/importerviewer.js
@@ -119,7 +119,7 @@ ImporterViewer.prototype.HighlightMesh = function (index, highlight)
 		if (current instanceof THREE.Mesh) {
 			if (current.originalJsonMeshIndex == index) {
 				if (highlight) {
-                    current.material.emissive.setHex (0x555555);
+                    // current.material.emissive.setHex (0x555555);
                     console.log(current.material);
                     const url = 'website/images/texture_sample.jpg';
                     // const url = 'https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwihtMnBrrnmAhXGFIgKHd9wAoYQjRx6BAgBEAQ&url=https%3A%2F%2Fdesignbundles.net%2Fhttpslinktreegulaydayi%2F51360-wood-texture-background&psig=AOvVaw30zHFsvSXras12yVxDPpzi&ust=1576558040351370';
